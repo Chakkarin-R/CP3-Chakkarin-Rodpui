e56a8c2525c95a93594c7fe95a6250014dc34aa7
Chakkarin-R <chakkarin.ro@gmail.com>
2025-10-14 14:33:28 +0700

    Create Exercise4_Chakarin_R.py

---
+++ Assignment/Exercise4_Chakarin_R.py
@@ -0,0 +1,10 @@
+FE = input() #Foundation English
+GB = input() #General Business
+ICS = input() #Introduction to Computer Systems
+CP = input() #Computer Programming
+
+print ("***** Your score *****")
+print ("Foundation English : " ,FE)
+print ("General Business :" ,GB)
+print ("Introduction to Computer Systems : " ,ICS)
+print ("Computer Programming : " ,CP)
