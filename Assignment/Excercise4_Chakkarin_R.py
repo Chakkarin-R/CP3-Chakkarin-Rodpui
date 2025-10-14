afe2079ae04ee27a825ee3400b21550b1019d014
Chakkarin-R <chakkarin.ro@gmail.com>
2025-10-14 13:38:26 +0700

    Update Excercise4_Chakkarin_R.py

---
+++ Assignment/Excercise4_Chakkarin_R.py
@@ -0,0 +1,9 @@
+FE = input() #Foundation English
+GB = input() #General Business
+ICS = input() #Introduction to Computer Systems
+CP = input () #Computer Programming
+
+print (FE)
+print (GB)
+print (ICS)
+print (CP)
